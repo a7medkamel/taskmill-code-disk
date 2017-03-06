73e0074db65b3ef32d7d7555fa3dd4bccdb41650
Ahmed Kamel <ak@ahmedkamel.net>
2017-03-06 00:36:46 -0800

    cleanups and app-module-path fallback

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ var _       = require('lodash')
   , Promise = require('bluebird')
   , path    = require('path')
   , vm      = require('vm')
+  , amp     = require('app-module-path')
   ;
+
+// do this once
+amp.addPath(path.join(process.cwd(), 'node_modules'));
 
 function Code(data) {
   this.data = data || {};
@@ -13,31 +17,42 @@ Code.prototype.run = function(req, res, next) {
     , filename  = this.data.filename
     ;
 
+    // todo [akamel] exception here doesn't return err to browser
   return Promise
           .try(() => {
-            if (!blob) {
-              return require(filename);
-            }
+            let _dirname  = path.resolve('/mnt/src/', path.dirname(filename))
+              , _filename = path.resolve('/mnt/src/', filename)
+              ;
 
-            var dirname   = path.dirname(filename)
-              // todo [akamel] require might have wrong lookup paths
-              , context   = vm.createContext(_.defaults({ module : {}, require : require, __direname : dirname, __filename : filename }, global))
-              , script    = new vm.Script(blob, { filename : filename })
+            // if (!blob) {
+              return Promise
+                      .try(() => {
+                        return require(_filename);
+                      })
+                      .catch((err) => {
+                        console.error(err);
+                        throw new Error(`module not found: ${filename}`);
+                      });
+            // }
+
+            // todo [akamel] require might have wrong lookup paths
+            let context   = vm.createContext(_.defaults({ module : {}, require : require, __dirname : _dirname, __filename : _filename }, global))
+              , script    = new vm.Script(blob, { filename : _filename })
               ;
 
             // todo [akamel] can throw 'Script execution timed out.' explain to user / otherwise hard to understand
             script.runInContext(context, { timeout : 2000 });
 
-            var fct = context.module.exports;
+            return context.module.exports;
           })
-          .catch((err) => {
-            throw new Error(`module not found: ${filename}`);
+          .catchThrow((err) => {
+            console.error(err);
           })
           .then((fct) => {
             if (_.isFunction(fct)) {
-              fct.call(req.app, req, res, next);
+              return fct.call(req.app, req, res, next);
             } else {
-              var err = new Error('module.exports not set to a function');
+              let err = new Error('module.exports not set to a function');
               err.help_url = 'https://taskmill.io/help';
 
               throw err;
