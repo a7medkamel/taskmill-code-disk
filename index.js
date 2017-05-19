18cd526004965ffd6793b59b9e48a4e951082c58
Ahmed Kamel <ak@ahmedkamel.net>
2017-05-19 01:49:29 -0700

    early support dot/flow

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var _       = require('lodash')
   , mime    = require('mime-types')
   , marked  = require('marked')
   , fse     = require('fs-extra')
+  , viz     = require('viz.js')
+  , dot     = require('dotparser')
   ;
 
 // do this once
@@ -12,8 +14,14 @@ amp.addPath(path.join(process.cwd(), 'node_modules'));
 
 // Synchronous highlighting with highlight.js 
 marked.setOptions({
-  highlight: function (code) {
-    return require('highlight.js').highlightAuto(code).value;
+  highlight: function (code, lang) {
+    switch(lang) {
+      case 'dot':
+      return viz(code);
+      break;
+      default:
+      return require('highlight.js').highlightAuto(code).value;
+    }
   }
 });
 
@@ -60,14 +68,25 @@ Code.prototype.run = function(req, res, next) {
                             return text;
                           }
 
+                          let options = { gfm : true };
+
+                          let md      = require('marked').lexer(text, options)
+                            , graph   = _.find(md, o => o.type == 'code' && o.lang == 'dot')
+                            , parsed  = dot(graph.text)
+                            ;
+
+
                           // style https://github.com/sindresorhus/github-markdown-css
                           return Promise
-                                  .fromCallback((cb) => marked(text, { gfm : true }, cb))
+                                  .fromCallback((cb) => marked(text, options, cb))
                                   .then((part) => {
                                     return `<html>
                                               <head>
                                                 <style>
                                                   @import url("https://rawgit.com/sindresorhus/github-markdown-css/gh-pages/github-markdown.css");
+                                                  @import url("https://rawgit.com/isagalaev/highlight.js/master/src/styles/github.css");
+
+                                                  @import url("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
 
                                                   body {
                                                     box-sizing: border-box;
@@ -76,9 +95,48 @@ Code.prototype.run = function(req, res, next) {
                                                     margin: 0 auto;
                                                     padding: 45px;
                                                   }
+
+                                                  ul {
+                                                    margin: 0;
+                                                  }
+
+                                                  body { padding-bottom: 70px; }
                                                 </style>
+                                                <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+                                                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                                                <script>
+                                                  $(function(){
+                                                    let elm = $('#flow-run');
+
+
+                                                    
+                                                    // window.dot = elm.find('pre').text();
+
+                                                    elm.click(function(e) {
+                                                      e.preventDefault();
+
+                                                      $.post({
+                                                          url : 'http://' + window.location.hostname + ':8030' + window.location.pathname
+                                                      });
+                                                      // console.log(window.dot);
+                                                    });
+                                                  })
+                                                </script>
                                               </head>
-                                              <body class='markdown-body'>${part}</body>
+                                              <body class='markdown-body'>
+                                                ${part}
+                                                <pre style='display:block;'>${JSON.stringify(parsed, null, ' ')}</pre>
+                                                <nav class="navbar navbar-default navbar-fixed-bottom navbar-inverse">
+                                                  <div class="container">
+                                                    <div class="navbar-header">
+                                                      <a class="navbar-brand" href="#">Breadboard</a>
+                                                    </div>
+                                                    <ul class="nav navbar-nav navbar-right">
+                                                      <li><button id="flow-run" type="button" class="btn btn-default navbar-btn">Run</button></li>
+                                                    </ul>
+                                                  </div>
+                                                </nav>
+                                              </body>
                                             </html>`;
                                   })
                                   ;
