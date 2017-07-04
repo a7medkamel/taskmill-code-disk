f429eb76b5ff3937b0151fd859dce723b4236f8e
Ahmed Kamel <ak@ahmedkamel.net>
2017-07-04 02:37:03 -0700

    parse manual

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var _       = require('lodash')
   , Promise = require('bluebird')
   , path    = require('path')
@@ -8,6 +10,7 @@ var _       = require('lodash')
   , viz     = require('viz.js')
   , dot     = require('dotparser')
   , pug     = require('pug')
+  , man     = require('taskmill-core-man')
   ;
 
 // do this once
@@ -26,68 +29,90 @@ marked.setOptions({
   }
 });
 
-function Code(data = {}) {
-  this.data = data;
-};
+class Code {
+  constructor (data = {}) {
+    this.data = data;
+  }
 
-Code.prototype.run = function(req, res, next) {
-  // return res.send({ path : req.path, base_url : req.get('base_url') });
-  // let base_url = req.get('base_url');
-  // let filename = path.relative(base_url, req.path);
-  let filename = path.relative('/', req.path);
+  static func_info(req) {
+    let name = path.relative('/', req.path);
 
-  // todo [akamel] exception here doesn't return err to browser
-  return Promise
-          .try(() => {
-            let _filename = path.resolve('/mnt/src/', filename)
-              , mime_type = mime.lookup(_filename)
-              ;
+    return {
+        name
+      , filename  : path.resolve('/mnt/src/', name)
+      , mime_type : mime.lookup(name)
+    };
+  }
 
-            switch(mime_type) {
-              case 'application/javascript':
-                let fct = require(_filename);
+  static text(req) {
+    let { filename } = Code.func_info(req);
 
-                if (_.isFunction(fct)) {
-                  return fct(req, res, next);
-                } else {
-                  let err = new Error('module.exports not set to a function');
-                  err.help_url = 'https://taskmill.io/help';
+    return Promise.fromCallback((cb) => fse.readFile(filename, 'utf8', cb));
+  }
 
-                  throw err;
-                }
-              break;
-              case 'text/x-markdown':
-                return Promise
-                        .fromCallback((cb) => fse.readFile(_filename, 'utf8', cb))
-                        .then((text) => {
-                          let pref = req.accepts(['text/x-markdown', 'text/html']);
+  static man(req) {
+    return Code
+            .text(req)
+            .then((text) => {
+              return man.get(text);
+            });
+  }
 
-                          if (pref == 'text/x-markdown') {
-                            return text;
-                          }
+  run(req, res, next) {
+    // return res.send({ path : req.path, base_url : req.get('base_url') });
+    // let base_url = req.get('base_url');
+    // let filename = path.relative(base_url, req.path);
+    let { filename, mime_type } = Code.func_info(req);
 
-                          let options = { gfm : true };
+    // todo [akamel] exception here doesn't return err to browser
+    return Promise
+            .try(() => {
+              switch(mime_type) {
+                case 'application/javascript':
+                  let fct = require(filename);
 
-                          let md      = require('marked').lexer(text, options)
-                            , graph   = _.find(md, o => o.type == 'code' && o.lang == 'dot')
-                            , parsed  = dot(graph.text)
-                            ;
+                  if (_.isFunction(fct)) {
+                    return fct(req, res, next);
+                  } else {
+                    let err = new Error('module.exports not set to a function');
+                    err.help_url = 'https://taskmill.io/help';
 
-                          // style https://github.com/sindresorhus/github-markdown-css
-                          return Promise
-                                  .fromCallback((cb) => marked(text, options, cb))
-                                  .then((part) => {
-                                    return pug.renderFile(path.join(__dirname, './view/markdown.pug'), { markdown : part })
-                                  });
-                        })
-                        .then((html) => res.send(html));
-              break;
-              case 'text/html':
-              default:
-                return Promise.fromCallback((cb) => res.sendFile(_filename, cb));
-              break;
-            }
-          });
-};
+                    throw err;
+                  }
+                break;
+                case 'text/x-markdown':
+                  return Code
+                          .text(req)
+                          .then((text) => {
+                            let pref = req.accepts(['text/x-markdown', 'text/html']);
+
+                            if (pref == 'text/x-markdown') {
+                              return text;
+                            }
+
+                            let options = { gfm : true };
+
+                            let md      = require('marked').lexer(text, options)
+                              , graph   = _.find(md, o => o.type == 'code' && o.lang == 'dot')
+                              , parsed  = dot(graph.text)
+                              ;
+
+                            // style https://github.com/sindresorhus/github-markdown-css
+                            return Promise
+                                    .fromCallback((cb) => marked(text, options, cb))
+                                    .then((part) => {
+                                      return pug.renderFile(path.join(__dirname, './view/markdown.pug'), { markdown : part })
+                                    });
+                          })
+                          .then((html) => res.send(html));
+                break;
+                case 'text/html':
+                default:
+                  return Promise.fromCallback((cb) => res.sendFile(filename, cb));
+                break;
+              }
+            });
+  }
+}
 
 module.exports = Code;
